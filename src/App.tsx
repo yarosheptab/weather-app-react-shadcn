22e214c8053ca1d031cc970ce46cd7fd3cc8883c
Bohdan Yaroshepta <yaroshepta.b@gmail.com>
2025-07-21 23:11:40 +0300

    feat: Installed shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { ThemeProvider } from '@/providers/theme-provider';
+
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello World</h1>;
+	return (
+		<ThemeProvider defaultTheme="dark">
+			<div>
+				<h1 className="text-3xl font-bold underline">Hello World</h1>
+				<Button variant="outline">Click me</Button>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
